c38f0fb764b9db9e3ae1bd0ee386095b4f8cc6cb
Patricia Novaes <patricia.novaes@gmail.com>
2022-03-27 15:01:33 -0300

    First Commit

---
+++ src/main/java/br/com/letscode/loja/LojaDbApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaDbApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/letscode/loja/produto/ProdutoEntidade.java
@@ -0,0 +1,4 @@
+package br.com.letscode.loja.produto;
+
+public class ProdutoEntidade {
+}
